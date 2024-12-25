4d4be9f768f3616aa43aec7f5d495984d5fd47a8
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-25 14:12:36 +0530

    data ingestion pipeline added

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from ml_complete_proj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ml_complete_proj import logger
 
-# logger.info('Welcome to custom log!!!')
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
