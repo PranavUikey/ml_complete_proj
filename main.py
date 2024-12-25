9d982939499b57efd0ef6b22fe7816c345ad2a31
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-26 00:09:11 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ml_complete_proj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ml_complete_proj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ml_complete_proj import logger
 
 
@@ -14,3 +15,15 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = "Data Validation stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
