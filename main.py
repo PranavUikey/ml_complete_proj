90d204fc8c00ec1ed2d0d25edf2697a834484925
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-26 18:38:33 +0530

    model trainer pipeline added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ml_complete_proj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ml_complete_proj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ml_complete_proj.pipeline.stage_03_data_transformation  import DataTransformationTrainingPipeline
+from ml_complete_proj.pipeline.stage_04_model_training import ModelTrainingPipeline
 from ml_complete_proj import logger
 
 
@@ -35,6 +36,18 @@ STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
