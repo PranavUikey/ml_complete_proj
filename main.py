fb7fe16613059fcf0826efeadd005b7dbc8fe792
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-26 10:21:19 +0530

    data_transformation_pipeline added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ml_complete_proj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ml_complete_proj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from ml_complete_proj.pipeline.stage_03_data_transformation  import DataTransformationTrainingPipeline
 from ml_complete_proj import logger
 
 
@@ -22,6 +23,18 @@ STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
